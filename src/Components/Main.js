ed4210778e7c3e1f6346fade2fdf9b77f7dab9fa
Shivranjan Kumar <shivranjan77@gmail.com>
2020-08-07 20:16:16 +0530

    All Ready Else Comment Part

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -9,51 +9,113 @@ export default class Main extends Component {
         super(props)
     
         this.state = {
-            text:""
-
-            
-             
-        }
-       // this.postHandler=this.postHandler.bind(this);
+            title:'',
+            Description:'',
+            items:[]     
+        }  
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const itemsRef = fire.database().ref('items');
+    const item = {
+      title1: this.state.title,
+      Description1: this.state.Description
+    }
+    itemsRef.push(item);
+    this.setState({
+      title1: '',
+      Description1: ''
+    });
+  }
+  componentDidMount() {
+    const itemsRef = fire.database().ref('items');
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          title1: items[item].title1,
+          Description1: items[item].Description1
+        });
+      }
+      this.setState({
+        items: newState
+      });
+    });
+  }
+  removeItem(itemId) {
+    const itemRef = fire.database().ref(`/items/${itemId}`);
+    itemRef.remove();
+  }
+  
+  
     logout(){
         fire.auth().signOut();
-    }
-    post(event){
-        this.setState({
-            text:event.target.value
-        })
-    }
-     
-   saveHandle=e=>{
-        let messageRef= fire.database().ref().orderByKey().limitToLast(100);
-        fire.database().ref('post').push(this.state.text);
-            console.log(messageRef);
-            alert("Data Save to Firebase Database You can check on console and expand query fetching is not done till this message");
-    }
+    } 
     render() {
+       
         return (
       
            <>
-            <div>
+            <div className="app">
               <h1 className="main"> Welcome</h1>
               
-               <input type="text-area" value={this.state.text} onChange={this.post.bind(this)}></input>
-           
-              <button className="post" onClick={this.saveHandle} >Create Post</button><br></br>
             </div>
              
              
              <div>
               <br></br><button onClick={this.logout}>Logout</button>
               </div>
+              <div className='container'>
+              <section className='add-item'>
+                <form onSubmit={this.handleSubmit}>
+                  <input type="text" name="title" placeholder="Title" onChange={this.handleChange} value={this.state.title} />
+                  <input type="text" name="Description" placeholder="Description" onChange={this.handleChange} value={this.state.Description} /><br></br>
+                  <button>Create New Post</button>
+                </form>
+          </section>
+          <section className='display-item'>
+              <div className="wrapper">
+              <h2>All Posts</h2>
+                <ul>
+                  {this.state.items.map((item) => {
+                    return ( 
+                        <div className="card">
+                         
+                        <h3>Title- {item.title1}</h3>
+                        <p>{item.Description1}
+                        <br></br>
+                          <button onClick={() => this.removeItem(item.id)}>Remove Post</button>
+                          
+                        </p>
+                      
+                      </div> 
+                    )
+                  })}
+                  
+                </ul>
+              </div>
+          </section>
+        </div>
+        </>
+      
+    );
              
               
             
             
-            </>
+           
             
-        )
+        
     }
     
 }
